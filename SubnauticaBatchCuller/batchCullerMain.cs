b4e4ac8398e77c0a57fefe2e03c0c062c334207d
Zigglegarf <zhembree654@gmail.com>
2017-01-14 11:54:39 -0500

    Removed Redundant Autogenerated Method

--- SubnauticaBatchCuller/batchCullerMain.cs
+++ SubnauticaBatchCuller/batchCullerMain.cs
@@ -42,19 +42,5 @@ namespace SubnauticaBatchCuller
 			tooltips = selector.tooltips;
             grid = selector.grid;
         }
-
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CullerForm));
-            this.SuspendLayout();
-            // 
-            // CullerForm
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "CullerForm";
-            this.ResumeLayout(false);
-
-        }
     }
 }
